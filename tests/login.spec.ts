2cdec444e5ee5d38b9d1db4534019cd65d016820
Pawel Kumanowski <pkumanowski@outlook.com>
2022-06-04 17:46:33 +0200

    fixed broken login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 const Chance = require('chance');
 const chance = new Chance();
-const name = chance.word({syllables: 2});
+const name = chance.word({syllables: 3});
 const password = chance.word({syllables: 2})
 
 test.beforeEach(async ({ page }) => {
@@ -14,15 +14,16 @@ test.describe('Login test', () => {
       await expect(page.locator('[id="signin2"]')).toBeVisible()
       await  page.locator('[id="signin2"]').click({force: true})
       await expect(page.locator('[id="signInModalLabel"]')).toBeVisible()
-      await page.locator('[id="sign-username"]').type(name)
-      await page.locator('[id="sign-password"]').type(password, {delay: 50})
+      await page.waitForTimeout(1000)
+      await page.locator('[id="sign-username"]').type(name, {delay: 100})
+      await page.locator('input[type="password"][id="sign-password"] ').type(password, {delay: 100})
       await page.locator('[onclick="register()"]').click()
-      page.on('dialog', dialog => dialog.accept());
       await expect(page.locator('[id="tbodyid"]')).toBeVisible();
   });
-  test('zlogin', async ({ page }) => {
+  test('login', async ({ page }) => {
       await page.locator('[data-target="#logInModal"]').click()
       await expect(page.locator('[id="logInModalLabel"]')).toBeVisible()
+      await page.waitForTimeout(1000)
       await page.locator('[id="loginusername"]').type(name)
       await page.locator('[id="loginpassword"]').type(password)
       await page.locator('[onclick="logIn()"]', {hasText: 'Log in'}).click({force: true})
